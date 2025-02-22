fb92f1c5907e890974174b89da58f98f01885ef0
pavvelpotapov <pavelminsk1979@mail.ru>
2025-02-22 04:15:35 +0300

    save image

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,8 +6,8 @@ import {
   UseInterceptors,
 } from '@nestjs/common';
 import { AppService } from './app.service';
-import fs from 'node:fs';
-import path from 'node:path';
+import * as fs from 'node:fs';
+import * as path from 'node:path';
 import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller()
@@ -24,6 +24,22 @@ export class AppController {
   getImage(@UploadedFile() imageimage: Express.Multer.File) {
     console.log('imageimage', imageimage);
 
-    return '1';
+    const dirPath = path.join(__dirname, 'content', '10');
+
+    const filePath = path.join(dirPath, imageimage.originalname);
+
+    if (!fs.existsSync(dirPath)) {
+      fs.mkdirSync(dirPath, { recursive: true });
+    }
+
+    fs.writeFile(filePath, imageimage.buffer, (err) => {
+      if (err) {
+        console.error('Ошибка при сохранении файла:', err);
+        return '400 not save image';
+      }
+      console.log('Файл успешно сохранен:', filePath);
+    });
+
+    return '200:ok!';
   }
 }
